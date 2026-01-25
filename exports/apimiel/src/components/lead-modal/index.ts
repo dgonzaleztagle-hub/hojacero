451cfd7a0e0825d3fa120727509d9c5244218a08
Daniel González <dgonzalez.tagle@gmail.com>
2026-01-25 19:44:47 -0300

    chore: ignorar carpeta exports (se guardan en repos independientes por cliente)

--- exports/apimiel/src/components/lead-modal/index.ts
+++
@@ -1,5 +0,0 @@
-// Lead Modal Tab Components - Barrel Export
-export { ModalTabDiagnostico } from './ModalTabDiagnostico';
-export { ModalTabAuditoria } from './ModalTabAuditoria';
-export { ModalTabEstrategia } from './ModalTabEstrategia';
-export { ModalTabTrabajo } from './ModalTabTrabajo';
